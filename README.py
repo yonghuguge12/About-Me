a2942c027bdfe37bd4eb18c72a976f3c47d8ac03
Li Chengze0612 <72480182+yonghuguge12@users.noreply.github.com>
2020-11-21 14:01:18 +0800

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,8 @@
+from time import*
+print("I am a Chinese elementary school student.")
+sleep(1)
+print("My girlfriend let me in GitHub.")
+sleep(1)
+print("Do you understand it?")
+sleep(1)
+print("拜拜了您呐！")
